a8b8ab8e8105c47f0da8402a829e1d9cf7a8b144
isViolet <1976334188@qq.com>
2022-09-07 14:29:13 +0800

    新建第一个项目

---
+++ src/test/java/code/wjf/onlineedu/OnlineEduApplicationTests.java
@@ -0,0 +1,13 @@
+package code.wjf.onlineedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
